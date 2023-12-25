2fe1eb21a48816c18a45434536f524f59b55bd55
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-12-25 22:19:31 +0530

    Created a route to register a new user

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,39 @@
 import express, {Application, Request, Response} from 'express';
+const { PrismaClient } = require("@prisma/client")
+
 const app: Application = express();
+const prisma = new PrismaClient();
 
 const PORT: number = 8080;
 
-app.use("/", (req: Request, res: Response): void => {
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response): void => {
     res.send("hello world");
+});
+
+app.post("/register", async (req: Request, res: Response): Promise<void> => {
+    try {
+        console.log(req.body);
+        const newUser = await prisma.user.create({
+            data: req.body
+        });
+
+        res.json(newUser);
+    } catch(err) {
+        console.error("Error during registration:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get("/getUsers", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const allUsers = await prisma.user.findMany();
+        res.json(allUsers);
+    }catch(err){
+        console.error("Error during registration:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(PORT, (): void => {
