7e035d6133083f6e6fe796e65b5b581ec88e54bd
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2024-01-03 22:26:13 +0530

    Added home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 import React, {useEffect} from 'react';
-import { Inter } from 'next/font/google'
-import { Navbar } from './components/Navbar'
-import { Footer } from './components/Footer'
 
 export default function Home() {
   return (
-   <main>
-   </main>
+    <main className="flex justify-center items-center h-screen">
+    <div className="bg-blue-700 h-[22rem] w-[20rem] p-6 flex flex-col items-center justify-center text-white rounded-xl">
+      <h1 className="text-[1.5rem] underline underline-offset-8 mb-[2rem]">
+        Welcome to MemoEase!
+      </h1>
+      <p className="text-center">
+        Unlock the power of effortless organization and enhanced productivity with MemoEase. 
+        Seamlessly designed for simplicity and efficiency, MemoEase is your digital haven for note-taking and 
+        task management.
+      </p>
+    </div>
+  </main>
+  
   )
 }
